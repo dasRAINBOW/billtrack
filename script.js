e59c6e03a32c2e68cdd38015a24ead12fa3739e0
dasRAINBOW <0p0art0123@gmail.com>
2024-08-28 03:26:25 +0200

    start of the beginning

---
+++ script.js
@@ -0,0 +1,12 @@
+var elem = document.documentElement;
+
+function openFullscreen() {
+    if (elem.requestFullscreen) {
+        elem.requestFullscreen();
+    } else if (elem.webkitRequestFullscreen) { /* Safari */
+        elem.webkitRequestFullscreen();
+    } else if (elem.msRequestFullscreen) { /* IE11 */
+        elem.msRequestFullscreen();
+    }
+    document.getElementById("fullScreenPrompt").style.display = "none";
+}
