2efff541a14a01a7f95745dd10ea734156dd8e1a
dasRAINBOW <0p0art0123@gmail.com>
2024-08-30 02:30:03 +0200

    The Looks Update

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
-var elem = document.documentElement;
+const fullScreenPrompt = document.getElementById('fullScreenPrompt');
+const themeToggle = document.getElementById('theme-toggle');
+const root = document.documentElement;
 
-function openFullscreen() {
-    if (elem.requestFullscreen) {
-        elem.requestFullscreen();
-    } else if (elem.webkitRequestFullscreen) { /* Safari */
-        elem.webkitRequestFullscreen();
-    } else if (elem.msRequestFullscreen) { /* IE11 */
-        elem.msRequestFullscreen();
-    }
-    document.getElementById("fullScreenPrompt").style.display = "none";
+fullScreenPrompt.addEventListener('click', () => {
+        const requestFullscreen = root.requestFullscreen || root.webkitRequestFullscreen || root.msRequestFullscreen;
+        if (requestFullscreen) requestFullscreen.call(root);
+        fullScreenPrompt.style.display = 'none';
+    },
+    { once: true }
+);
+
+themeToggle.addEventListener('click', function () {
+    const isLightTheme = root.getAttribute('data-theme') === 'light';
+    root.setAttribute('data-theme', isLightTheme ? 'dark' : 'light');
+    this.textContent = isLightTheme ? '☀' : '☽';
+});
+
+function checkTheme() {
+    root.setAttribute('data-theme', 'dark');
 }
