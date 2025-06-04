c4dae780d4fa9cd51174a99c8407f73b9af9b69a
dasRAINBOW <69715079+dasRAINBOW@users.noreply.github.com>
2025-06-04 08:50:10 +0200

    Fixed broken icon links in history

--- history/script.js
+++ history/script.js
@@ -10,7 +10,7 @@ themeToggle.addEventListener('click', function () {
     root.setAttribute('data-theme', isLightTheme ? 'dark' : 'light');
 
     // Update the button text to reflect the current theme
-    document.getElementById('themeIcon').setAttribute('src', isLightTheme ? './icons/sun.svg' : './icons/moon.svg');
+    document.getElementById('themeIcon').setAttribute('src', isLightTheme ? '../icons/sun.svg' : '../icons/moon.svg');
     document.getElementById('themeIcon').setAttribute('class', isLightTheme ? '' : 'inverted');
     document.getElementById('saveIcon').setAttribute('class', isLightTheme ? '' : 'inverted');
 });
